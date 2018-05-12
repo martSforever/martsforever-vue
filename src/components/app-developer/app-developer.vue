5ada8d01561f2e9b7c0bc0bebca8e33373686384
weishengjian <shengjian.wei@hand-china.com>
2018-05-12 21:18:55 +0800

    成功引入iconfront

--- src/components/app-developer/app-developer.vue
+++ src/components/app-developer/app-developer.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="app-developer">
     this is developer page
+    <svg class="icon iconfront" aria-hidden="true">
+      <use xlink:href="#icon-shouhoufuwurexian"></use>
+    </svg>
   </div>
 </template>
 
@@ -19,5 +22,8 @@
 </script>
 
 <style scoped lang="stylus">
-
+  .iconfront {
+    color rebeccapurple
+    font-size 100px
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App'
 import router from './router'
 import 'font-awesome/css/font-awesome.css';         //font-awesome
 import 'swiper/dist/css/swiper.css';                //swiper
-
+import 'src/libs/iconfront/iconfont.js';            //iconfront
 import "src/base/style/theme.styl"
 
 Vue.config.productionTip = false
