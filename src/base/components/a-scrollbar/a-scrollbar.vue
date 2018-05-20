88cf12b67d99c675f8e76f4a81f3643c2615deac
weishengjian <shengjian.wei@hand-china.com>
2018-05-20 16:23:37 +0800

    基本完善了自定义滚动条的功能

--- src/base/components/a-scrollbar/a-scrollbar.vue
+++ src/base/components/a-scrollbar/a-scrollbar.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="a-scrollbar" ref="scrollbar">
-    <div class="a-scrollbar-content" ref="scrollbarContent" @scroll="_onContentVScroll">
+  <div class="a-scrollbar" ref="scrollbarWrapper">
+    <div class="a-scrollbar-content" ref="scrollbarContent" @scroll="_onScroll">
       <a-button @click="log">log</a-button>
       <slot></slot>
     </div>
-    <div class="a-scrollbar-h" ref="vScrollbar">
-      <div class="a-scroll-bar-h-indicator" ref="vIndicator"></div>
+    <div class="a-scrollbar-v" ref="vScrollbar" v-show="vShowScrollbar">
+      <div class="a-scroll-bar-v-indicator" ref="vIndicator"></div>
+    </div>
+    <div class="a-scrollbar-h" ref="hScrollbar">
+      <div class="a-scroll-bar-h-indicator" ref="hIndicator"></div>
     </div>
   </div>
 </template>
@@ -24,7 +27,7 @@
       },
       scrollBarColor: {
         type: String,
-        default: '#000',
+        default: 'rgba(0,0,0,0.5)',
         desc: '滚动条背景色'
       },
       scrollBarSize: {
@@ -46,8 +49,10 @@
     name: "a-scrollbar",
     data() {
       return {
-        vTouch: {},
-        showScrollBar: false
+        vTouch: {},                   //纵向滚动时，用来存储一些数据的对象
+        hTouch: {},                   //横向滚动时，用来存储一些数据的对象
+        vShowScrollbar: false,        //纵向滚动条是否显示
+        hShowScrollbar: true,         //横向滚动条是否显示
       }
     },
     mounted() {
@@ -56,83 +61,184 @@
     computed: {},
     methods: {
       log() {
-        console.dir(this.$refs.scrollbarContent.scrollHeight);
+        console.dir(this.$refs.scrollbarContent);
       },
       update() {
         console.log('update');
-        this._updateVSize();
+        this._vUpdateSize();
+        this._hUpdateSize();
       },
       _initialize() {
         this.$nextTick(function () {
-          this._initializeHScrollbar();
-          this._initializeVScrollbar();
+          this._watchContentSize();
+          this._hInitializeScrollbar();
+          this._vInitializeScrollbar();
+          document.addEventListener('mousemove', this._onMousemove);
+          document.addEventListener('mouseup', this._onMouseup);
+          this._vUpdateSize();
+          this._hUpdateSize();
         });
       },
-      _initializeHScrollbar() {
-      },
       /*初始化纵向滚动条*/
-      _initializeVScrollbar() {
-        // console.log('_initializeVScrollbar');
-        this._updateVSize();
-        this._watchContentSize();
-        this.$refs.vIndicator.addEventListener('mousedown', this._onVIndicatorMouseDown);
-        document.addEventListener('mousemove', this._onMousemove);
-        document.addEventListener('mouseup', this._onMouseup);
-      },
-      /*更新纵向滚动条高度*/
-      _updateVSize() {
-        this.$refs.scrollbarContent.style.width = `${this.$refs.scrollbar.offsetWidth + 17}px`;
+      _vInitializeScrollbar() {
+        /*内容宽度增加17个像素，把滚动条隐藏*/
+        this.$refs.scrollbarContent.style.width = `${this.$refs.scrollbarWrapper.offsetWidth + 17}px`;
+        /*滚动条背景色，宽高位置，圆角设置*/
         this.$refs.vScrollbar.style.backgroundColor = this.scrollBarColor;
         this.$refs.vScrollbar.style.width = `${this.scrollBarSize}px`;
         this.$refs.vScrollbar.style.top = `${this.scrollBarSize}px`;
         this.$refs.vScrollbar.style.bottom = `${this.scrollBarSize}px`;
-
-
         this.$refs.vScrollbar.style.borderRadius = `${this.scrollbarRadius}px`;
-
-        this.$refs.vIndicator.style.height = `${this._calculateVIndicatorSize()}px`;
+        /*滚动条指示器设置*/
         this.$refs.vIndicator.style.backgroundColor = this.indicatorColor;
         this.$refs.vIndicator.style.borderRadius = `${this.scrollbarRadius}px`;
+        this.$refs.vIndicator.addEventListener('mousedown', this._vOnIndicatorMouseDown);
+      },
+      /*更新纵向滚动条高度*/
+      _vUpdateSize() {
+        this.$refs.vIndicator.style.height = `${this._vCalculateIndicatorSize()}px`;
       },
       /*计算纵向滚动条指示器高度*/
-      _calculateVIndicatorSize() {
-        if (!this.$refs.scrollbarContent) return 0;
-        let contentOffsetHeight = this.$refs.scrollbarContent.offsetHeight;
+      _vCalculateIndicatorSize() {
+        /*内容咩有初始化的时候，直接返回0*/
+        if (!this.$refs.scrollbarContent) {
+          this.vShowScrollbar = false;
+          return 0;
+        }
+        /*内容可视高度*/
+        let contentOffsetHeight = this.$refs.scrollbarContent.offsetHeight - 17;
+        /*内容滚动实际高度*/
         let contentScrollHeight = this.$refs.scrollbarContent.scrollHeight;
-        let scrollbarOffsetHeight = this.$refs.vScrollbar.offsetHeight;
-        return scrollbarOffsetHeight * contentOffsetHeight / contentScrollHeight;
+        console.log('contentOffsetHeight', contentOffsetHeight);
+        console.log('contentScrollHeight', contentScrollHeight);
+
+        /*当内容滚动高度与可视高度相等时（内容高度不足容器显示高度），不显示滚动条*/
+        this.vShowScrollbar = !(contentScrollHeight === contentOffsetHeight);
+
+        /*滚动条高度*/
+        //貌似是因为用了v-show控制滚动条显隐，导致滚动条第一次显示的时候，获取得到的offsetHeight一直都是0，这里
+        //换一种方法，通过内容可视高度减去滚动条宽度来获取滚动条高度，因为可能会存在纵向滚动条以及横向滚动条，所以纵向滚动条的上下部都会缩短滚动条宽度的距离，横向滚动条也一样，缩短一些距离
+        // let scrollbarOffsetHeight = this.$refs.vScrollbar.offsetHeight;
+        let scrollbarOffsetHeight = (this.$refs.scrollbarContent.offsetHeight) - (2 * this.scrollBarSize);
+        /*指示器高度*/
+        let indicatorHeight = scrollbarOffsetHeight * contentOffsetHeight / contentScrollHeight;
+        console.log('scrollbarOffsetHeight', scrollbarOffsetHeight);
+        console.log('indicatorHeight', indicatorHeight);
+        return indicatorHeight;
       },
       /*纵向滚动触发事件*/
-      _onContentVScroll(e) {
+      _vOnContentScroll(e) {
+        /*内容滚动位置*/
         let contentScrollTop = this.$refs.scrollbarContent.scrollTop;
-        let scrollBarScrollTop = (contentScrollTop / this.$refs.scrollbarContent.scrollHeight) * this.$refs.vScrollbar.offsetHeight;
+        let scrollBarScrollTop = (contentScrollTop / (this.$refs.scrollbarContent.scrollHeight - this.$refs.scrollbarContent.offsetHeight + 17)) * (this.$refs.vScrollbar.offsetHeight - this.$refs.vIndicator.offsetHeight);
         this.$refs.vIndicator.style.top = `${scrollBarScrollTop}px`;
       },
       /*纵向指示器点击开始*/
-      _onVIndicatorMouseDown(e) {
+      _vOnIndicatorMouseDown(e) {
         e.preventDefault();
         this.vTouch.initialized = true;
         this.vTouch.startX = e.clientX;
         this.vTouch.startY = e.clientY;
         this.vTouch.startTop = removePx(this.$refs.vIndicator.style.top);
+        /*内容滚动高度最大为：内容滚动高度减去内容可视高度，同理滚动条的滚动高度也是一样*/
         this.vTouch.maxTop = this.$refs.vScrollbar.offsetHeight - this.$refs.vIndicator.offsetHeight;
       },
-      /*纵向指示器点击移动*/
+      _vOnIndicatorMove(e) {
+        /*纵向指示器点击移动*/
+        if (!!this.vTouch.initialized) {
+          const deltaY = e.clientY - this.vTouch.startY;
+          /*指示器滚动*/
+          let indicatorResultTop = Math.min(Math.max(this.vTouch.startTop + deltaY, 0), this.vTouch.maxTop);
+          this.$refs.vIndicator.style.top = `${indicatorResultTop}px`;
+          /*内容滚动*/
+          let scrollContentResultTop = (indicatorResultTop / (this.$refs.vScrollbar.offsetHeight - this.$refs.vIndicator.offsetHeight)) * (this.$refs.scrollbarContent.scrollHeight - this.$refs.scrollbarContent.offsetHeight + 17);
+          this.$refs.scrollbarContent.scrollTop = `${scrollContentResultTop}`;
+        }
+      },
+
+      _hInitializeScrollbar() {
+        /*内容宽度增加17个像素，把横向滚动条隐藏*/
+        this.$refs.scrollbarContent.style.height = `${this.$refs.scrollbarWrapper.offsetHeight + 17}px`;
+        /*滚动条背景色，宽高位置，圆角设置*/
+        this.$refs.hScrollbar.style.backgroundColor = this.scrollBarColor;
+        this.$refs.hScrollbar.style.height = `${this.scrollBarSize}px`;
+        this.$refs.hScrollbar.style.left = `${this.scrollBarSize}px`;
+        this.$refs.hScrollbar.style.right = `${this.scrollBarSize}px`;
+        this.$refs.hScrollbar.style.borderRadius = `${this.scrollbarRadius}px`;
+        /*滚动条指示器设置*/
+        this.$refs.hIndicator.style.backgroundColor = this.indicatorColor;
+        this.$refs.hIndicator.style.borderRadius = `${this.scrollbarRadius}px`;
+        this.$refs.hIndicator.addEventListener('mousedown', this._hOnIndicatorMouseDown);
+      },
+      _hUpdateSize() {
+        this.$refs.hIndicator.style.width = `${this._hCalculateIndicatorSize()}px`;
+      },
+      _hCalculateIndicatorSize() {
+        /*内容咩有初始化的时候，直接返回0*/
+        if (!this.$refs.scrollbarContent) {
+          this.hShowScrollbar = false;
+          return 0;
+        }
+        /*内容可视宽度度*/
+        let contentOffsetWidth = this.$refs.scrollbarContent.offsetWidth - 17;
+        /*内容滚动实际高度*/
+        let contentScrollWidth = this.$refs.scrollbarContent.scrollWidth;
+        /*当内容滚动高度与可视高度相等时（内容高度不足容器显示高度），不显示滚动条*/
+        this.hShowScrollbar = !(contentScrollWidth === contentOffsetWidth);
+        /*滚动条长度*/
+        //貌似是因为用了v-show控制滚动条显隐，导致滚动条第一次显示的时候，获取得到的offsetWidth一直都是0，这里
+        //换一种方法，通过内容可视宽度减去滚动条大小（这里是高度）来获取滚动条长度，因为可能会存在纵向滚动条以及横向滚动条，所以纵向滚动条的上下部都会缩短滚动条宽度的距离，横向滚动条也一样，缩短一些距离
+        // let scrollbarOffsetHeight = this.$refs.vScrollbar.offsetHeight;
+        let scrollbarOffsetWdith = this.$refs.scrollbarContent.offsetWidth - (2 * this.scrollBarSize);
+        /*指示器宽度*/
+        let indicatorWidth = scrollbarOffsetWdith * contentOffsetWidth / contentScrollWidth;
+        return indicatorWidth;
+      },
+      /*横向滚动触发事件*/
+      _hOnContentScroll(e) {
+        /*内容滚动位置*/
+        let contentScrollLeft = this.$refs.scrollbarContent.scrollLeft;
+        /*指示器滚动位置*/
+        let scrollBarScrollLeft = (contentScrollLeft / (this.$refs.scrollbarContent.scrollWidth - this.$refs.scrollbarContent.offsetWidth + 17)) * (this.$refs.hScrollbar.offsetWidth - this.$refs.hIndicator.offsetWidth);
+        // console.log(this.$refs.scrollbarContent.scrollWidth, this.$refs.scrollbarContent.scrollLeft, this.$refs.scrollbarContent.offsetWidth);
+        this.$refs.hIndicator.style.left = `${scrollBarScrollLeft}px`;
+      },
+      /*纵向指示器点击开始*/
+      _hOnIndicatorMouseDown(e) {
+        e.preventDefault();
+        this.hTouch.initialized = true;
+        this.hTouch.startX = e.clientX;
+        this.hTouch.startY = e.clientY;
+        this.hTouch.startLeft = removePx(this.$refs.hIndicator.style.left);
+        /*内容滚动宽度最大为：内容滚动宽度减去内容可视宽度，同理滚动条的滚动宽度也是一样*/
+        this.hTouch.maxLeft = this.$refs.hScrollbar.offsetWidth - this.$refs.hIndicator.offsetWidth;
+      },
+      _hOnIndicatorMove(e) {
+        /*横向指示器移动*/
+        if (!!this.hTouch.initialized) {
+          const deltaX = e.clientX - this.hTouch.startX;
+          /*指示器滚动*/
+          let indicatorResultLeft = Math.min(Math.max(this.hTouch.startLeft + deltaX, 0), this.hTouch.maxLeft);
+          this.$refs.hIndicator.style.left = `${indicatorResultLeft}px`;
+          /*内容滚动*/
+          let scrollContentResultLeft = (indicatorResultLeft / (this.$refs.hScrollbar.offsetWidth - this.$refs.hIndicator.offsetWidth)) * (this.$refs.scrollbarContent.scrollWidth - this.$refs.scrollbarContent.offsetWidth + 17);
+          this.$refs.scrollbarContent.scrollLeft = `${scrollContentResultLeft}`;
+        }
+      },
+
+      _onScroll(e) {
+        this._vOnContentScroll(e);
+        this._hOnContentScroll(e);
+      },
       _onMousemove(e) {
         e.preventDefault();
-        if (!this.vTouch.initialized) return;
-        const deltaX = e.clientX - this.vTouch.startX;
-        const deltaY = e.clientY - this.vTouch.startY;
-        /*指示器滚动*/
-        let indicatorResultTop = Math.min(Math.max(this.vTouch.startTop + deltaY, 0), this.vTouch.maxTop);
-        this.$refs.vIndicator.style.top = `${indicatorResultTop}px`;
-        /*内容滚动*/
-        let scrollContentResultTop = (indicatorResultTop / this.$refs.vScrollbar.scrollHeight) * this.$refs.scrollbarContent.scrollHeight;
-        this.$refs.scrollbarContent.scrollTop = `${scrollContentResultTop}`;
+        this._vOnIndicatorMove(e);
+        this._hOnIndicatorMove(e);
       },
       /*纵向指示器点击结束*/
       _onMouseup(e) {
         this.vTouch.initialized = false;
+        this.hTouch.initialized = false;
       },
       _onMouseenter() {
         this.autoHide && (this.showScrollBar = true);
@@ -151,7 +257,8 @@
     beforeDestroy() {
       /*纵向滚动事件清除*/
       // console.log('beforeDestroy');
-      this.$refs.vIndicator.removeEventListener('mousedown', this._onVIndicatorMouseDown);
+      this.$refs.vIndicator.removeEventListener('mousedown', this._vOnIndicatorMouseDown);
+      this.$refs.hIndicator.removeEventListener('mousedown', this._hOnIndicatorMouseDown);
       document.removeEventListener('mousemove', this._onMousemove);
       document.removeEventListener('mouseup', this._onMouseup);
       this.observe.disconnect();
@@ -168,16 +275,27 @@
       @include fill-parent;
     }
     .a-scrollbar-content {
-      overflow-y: auto;
+      overflow-y: scroll;
+
+      overflow-x: scroll;
+      white-space: nowrap;
+    }
+    .a-scrollbar-v {
+      position: absolute;
+      right: 0;
+      .a-scroll-bar-v-indicator {
+        position: relative;
+        cursor: pointer;
+        width: 100%;
+      }
     }
     .a-scrollbar-h {
       position: absolute;
-      right: 0;
-      top: 0;
+      bottom: 0;
       .a-scroll-bar-h-indicator {
-        width: 100%;
         position: relative;
-        box-shadow: 0 0 8px $text-color-main;
+        cursor: pointer;
+        height: 100%;
       }
     }
   }
--- src/example/a-scrollbar/a-scrollbar-example.vue
+++ src/example/a-scrollbar/a-scrollbar-example.vue
@@ -3,6 +3,7 @@
     <div>
       <a-button-group shape="round" style="margin-bottom: 20px">
         <a-button @click="add">add</a-button>
+        <a-button @click="add2">add2</a-button>
         <a-button @click="remove">remove</a-button>
         <a-button @click="update">update</a-button>
         <a-button @click="toggleIf">toggleIf</a-button>
@@ -17,11 +18,23 @@
           </div>
         </a-scrollbar>
       </div>
-      <!--   <div class="container">
-           <a-scrollbar ref="scrollbarb">
-             <div class="row" v-for="item in list">{{item}}</div>
-           </a-scrollbar>
-         </div>-->
+      <div class="container" v-if="showScrollBar">
+        <a-scrollbar ref="scrollbara" :scroll-bar-size="scrollbarSize">
+          <div class="row" v-for="(item,index) in list2">
+            {{index}}
+            <img :src="item.url"/>
+          </div>
+        </a-scrollbar>
+      </div>
+      <div class="container" v-if="showScrollBar">
+        <a-scrollbar ref="scrollbar" :scroll-bar-size="scrollbarSize">
+          <div class="col" v-for="(item,index) in list">
+            <img :src="item.url"/>
+            {{index}}
+          </div>
+        </a-scrollbar>
+      </div>
+
     </div>
   </div>
 </template>
@@ -41,13 +54,17 @@
     data() {
       let num = 20;
       let list = [];
+      let list2 = [];
       for (let i = 0; i < num; i++) {
         list.push({
           url: 'http://img3.redocn.com/tupian/20100113/xiaoliankatongxiangkuang_113352_small.jpg'
         });
       }
+      list2.push(list[0]);
+      list2.push(list[1]);
       return {
         list,
+        list2,
         scrollbarSize: 6,
         showScrollBar: true
       }
@@ -55,6 +72,11 @@
     methods: {
       add() {
         this.list.push({
+          url: `http://img3.redocn.com/tupian/20100113/xiaoliankatongxiangkuang_113352_small.jpg?time=${this.getTime()}`
+        });
+      },
+      add2() {
+        this.list2.push({
           url: `http://img3.redocn.com/tupian/20100113/xiaoliankatongxiangkuang_113352_small.jpg?time=${this.getTime()}`
         });
       },
@@ -80,13 +102,18 @@
     @include flex-center;
     flex-direction: column;
     .container {
-      height: 500px;
-      width: 500px;
+      height: 360px;
+      width: 400px;
       background-color: #495a80;
       display: inline-block;
       .row {
         margin-bottom: 12px;
         background-color: blueviolet;
+        width: 600px;
+      }
+      .col {
+        display: inline-block;
+        width: max-content;
       }
     }
   }
