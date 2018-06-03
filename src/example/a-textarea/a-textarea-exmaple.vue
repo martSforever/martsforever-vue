bb73539eea60914e0b9cb1389b04ff061c14a5cf
weishengjian <shengjian.wei@hand-china.com>
2018-06-03 10:05:43 +0800

    增加滚动条显示

--- src/example/a-textarea/a-textarea-exmaple.vue
+++ src/example/a-textarea/a-textarea-exmaple.vue
@@ -1,41 +1,44 @@
 <template>
   <div class="a-textarea-example">
-    <a-title>普通输入框</a-title>
-    <div class="item"><a-textarea v-model="name"/></div>
-    <a-title>输入框大小</a-title>
-    <div class="item">
-      <a-textarea v-model="name" size="large"/>
-      <a-textarea v-model="name" size="middle"/>
-      <a-textarea v-model="name" size="small"/>
-    </div>
-    <a-title>输入框形状</a-title>
-    <div class="item">
-      <a-textarea v-model="name" shape="fillet"/>
-      <a-textarea v-model="name" shape="none"/>
-    </div>
+    <a-scrollbar>
+      <a-title>普通输入框</a-title>
+      <div class="item"><a-textarea v-model="name"/></div>
+      <a-title>输入框大小</a-title>
+      <div class="item">
+        <a-textarea v-model="name" size="large"/>
+        <a-textarea v-model="name" size="middle"/>
+        <a-textarea v-model="name" size="small"/>
+      </div>
+      <a-title>输入框形状</a-title>
+      <div class="item">
+        <a-textarea v-model="name" shape="fillet"/>
+        <a-textarea v-model="name" shape="none"/>
+      </div>
 
-    <a-title>可以试试往输入框粘贴图片</a-title>
-    <div class="item">
-      <div><a-textarea v-model="name" size="large"/></div>
-    </div>
-    <a-title>输入框颜色</a-title>
-    <div class="item">
-      <a-textarea v-model="name" size="small" color="primary"/>
-      <a-textarea v-model="name" size="small" color="info"/>
-      <a-textarea v-model="name" size="small" color="success"/>
-      <a-textarea v-model="name" size="small" color="warning"/>
-      <a-textarea v-model="name" size="small" color="error"/>
-    </div>
-
+      <a-title>可以试试往输入框粘贴图片</a-title>
+      <div class="item">
+        <div><a-textarea v-model="name" size="large"/></div>
+      </div>
+      <a-title>输入框颜色</a-title>
+      <div class="item">
+        <a-textarea v-model="name" size="small" color="primary"/>
+        <a-textarea v-model="name" size="small" color="info"/>
+        <a-textarea v-model="name" size="small" color="success"/>
+        <a-textarea v-model="name" size="small" color="warning"/>
+        <a-textarea v-model="name" size="small" color="error"/>
+      </div>
+    </a-scrollbar>
   </div>
 </template>
 
 <script>
   import ATextarea from "../../base/components/a-textarea/a-textarea";
   import ATitle from "../../base/components/a-title/a-title";
+  import AScrollbar from "../../base/components/a-scrollbar/a-scrollbar";
 
   export default {
     components: {
+      AScrollbar,
       ATitle,
       ATextarea
     },
