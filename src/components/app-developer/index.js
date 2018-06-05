da8fccdbc2dde7e88846a2cac383547727d972bc
weishengjian <shengjian.wei@hand-china.com>
2018-06-05 15:31:14 +0800

    路由懒加载配置

--- src/components/app-developer/index.js
+++ src/components/app-developer/index.js
@@ -1,27 +1,44 @@
-import AppDeveloper from 'src/components/app-developer/app-developer.vue';
-import AIconExample from 'src/example/a-icon/a-icon-example.vue';
-import AButtonExample from 'src/example/a-button/a-button-example.vue';
-import AInputExample from 'src/example/a-input/a-input-example';
-import AGridExample from 'src/example/a-grid/a-grid-example.vue';
-import AScrollbarExample from 'src/example/a-scrollbar/a-scrollbar-example.vue';
-import ATextareaExample from 'src/example/a-textarea/a-textarea-exmaple.vue';
-import ACarouselExample from 'src/example/a-carousel/a-carousel-example.vue';
-
 export const router = {
   path: '/developer',
-  component: AppDeveloper,
+  component: resolve => require(['src/components/app-developer/app-developer.vue'], resolve),
   children: [
-    {path: '/', component: AIconExample, name: 'AIconExample'},
-    {path: '/aButtonExample', component: AButtonExample, name: 'AButtonExample'},
-    {path: '/aInputExample', component: AInputExample, name: 'AInputExample'},
-    {path: '/aIconExample', component: AIconExample, name: 'AIconExample'},
-    {path: '/aGridExample', component: AGridExample, name: 'AGridExample'},
-    {path: '/aScrollbarExample', component: AScrollbarExample, name: 'AScrollbarExample'},
-    {path: '/aTextareaExample', component: ATextareaExample, name: 'ATextareaExample'},
-    {path: '/aCarouselExample', component: ACarouselExample, name: 'ACarouselExample'},
+    {
+      path: '/',
+      component: resolve => require(['src/example/a-icon/a-icon-example.vue'], resolve),
+      name: 'AIconExample'
+    },
+    {
+      path: '/aButtonExample',
+      component: resolve => require(['src/example/a-button/a-button-example.vue'], resolve),
+      name: 'AButtonExample'
+    },
+    {
+      path: '/aInputExample',
+      component: resolve => require(['src/example/a-input/a-input-example'], resolve),
+      name: 'AInputExample'
+    },
+    {
+      path: '/aGridExample',
+      component: resolve => require(['src/example/a-grid/a-grid-example.vue'], resolve),
+      name: 'AGridExample'
+    },
+    {
+      path: '/aScrollbarExample',
+      component: resolve => require(['src/example/a-scrollbar/a-scrollbar-example.vue'], resolve),
+      name: 'AScrollbarExample'
+    },
+    {
+      path: '/aTextareaExample',
+      component: resolve => require(['src/example/a-textarea/a-textarea-exmaple.vue'], resolve),
+      name: 'ATextareaExample'
+    },
+    {
+      path: '/aCarouselExample',
+      component: resolve => require(['src/example/a-carousel/a-carousel-example.vue'], resolve),
+      name: 'ACarouselExample'
+    },
   ]
 };
-
 export default {
   router
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import AppMain from 'src/components/app-main/app-main.vue'
-import AppBackground from 'src/components/app-setting/app-background.vue';
-import AppSystem from 'src/components/app-setting/app-system.vue';
-import AppUser from 'src/components/app-user/app-user.vue';
-import AppExample from 'src/components/app-example/app-example.vue';
-
 import AppDeveloper from 'src/components/app-developer'
 
 Vue.use(Router);
@@ -14,7 +8,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: AppMain,
+      component: resolve => require(['src/components/app-main/app-main.vue'],resolve),
       children: [
         {
           path: '/',
@@ -25,22 +19,22 @@ export default new Router({
         {
           path: '/appBackground',
           name: 'AppBackground',
-          component: AppBackground
+          component: resolve => require(['src/components/app-setting/app-background.vue'],resolve)
         },
         {
           path: '/appSystem',
           name: 'AppSystem',
-          component: AppSystem
+          component: resolve => require(['src/components/app-setting/app-system.vue'],resolve)
         },
         {
           path: '/appUser',
           name: 'AppUser',
-          component: AppUser
+          component: resolve => require(['src/components/app-user/app-user.vue'],resolve)
         },
         {
           path: '/appExample',
           name: 'AppExample',
-          component: AppExample
+          component: resolve => require(['src/components/app-example/app-example.vue'],resolve)
         }
       ]
     },
