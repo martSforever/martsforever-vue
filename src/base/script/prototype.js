6b1a9750f5c50c5b3edc4d47059e9d9df071466f
martsforever <martsforever@aliyun.com>
2018-08-23 10:30:50 +0800

    日期转换函数

--- src/base/script/prototype.js
+++ src/base/script/prototype.js
@@ -1,4 +1,4 @@
-import {oneOf} from "./utils";
+import {oneOf, zeroize} from "./utils";
 
 /*
 *  String字符串
@@ -108,5 +108,58 @@ Object.defineProperty(Array.prototype, 'unshiftArray', {
       this.unshift(item)
     })
     return this
+  }
+})
+
+Object.defineProperty(Date.prototype, 'format', {
+  value: function (formatStr) {
+    return formatStr.replace(/"[^"]*"|'[^']*'|\b(?:d{1,4}|M{1,4}|yy(?:yy)?|([hHmstT])\1?|[lLZ])\b/g, function ($0) {
+      switch ($0) {
+        case 'd':
+          return this.getDate();
+        case 'dd':
+          return zeroize(this.getDate());
+        case 'ddd':
+          return ['Sun', 'Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat'][this.getDay()];
+        case 'dddd':
+          return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][this.getDay()];
+        case 'M':
+          return this.getMonth() + 1;
+        case 'MM':
+          return zeroize(this.getMonth() + 1);
+        case 'MMM':
+          return ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][this.getMonth()];
+        case 'MMMM':
+          return ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][this.getMonth()];
+        case 'yy':
+          return new String(this.getFullYear()).substr(2);
+        case 'yyyy':
+          return this.getFullYear();
+        case 'h':
+          return this.getHours() % 12 || 12;
+        case 'hh':
+          return zeroize(this.getHours() % 12 || 12);
+        case 'H':
+          return this.getHours();
+        case 'HH':
+          return zeroize(this.getHours());
+        case 'm':
+          return this.getMinutes();
+        case 'mm':
+          return zeroize(this.getMinutes());
+        case 's':
+          return this.getSeconds();
+        case 'ss':
+          return zeroize(this.getSeconds());
+        case 'l':
+          return this.getMilliseconds();
+        case 'll':
+          return zeroize(this.getMilliseconds());
+        case 'tt':
+          return this.getHours() < 12 ? 'am' : 'pm';
+        case 'TT':
+          return this.getHours() < 12 ? 'AM' : 'PM';
+      }
+    });
   }
 })
