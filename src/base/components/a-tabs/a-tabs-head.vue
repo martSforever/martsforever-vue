8dae9c2b3e281b1c2a3e494b041710ec2b2af903
martsforever <martsforever@aliyun.com>
2018-08-25 10:48:07 +0800

    实现了在a-tabs中使用carousel，但是carousel好像还差点什么，加一个a-carousel-item，封装样式

---
+++ src/base/components/a-tabs/a-tabs-head.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="a-tabs-head">
+    <div class="a-tabs-head-item"
+         v-for="(item,index) in labels"
+         :key="index"
+         ref="items"
+         @mouseenter="_handleMouseEnter"
+         @mouseleave="_handleMouseLeave"
+         @click="_handleClickItem(item,index)">
+      {{item}}
+    </div>
+    <div class="a-tabs-head-indicator" :style="indicatorStyle"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "a-tabs-head",
+    props: {
+      labels: {
+        type: Array,
+        default: () => []
+      },
+      indicatorSize: {
+        type: Number,
+        default: 2
+      },
+      indicatorColor: {
+        type: String,
+        default: '#e03636'
+      },
+      value: {
+        type: Number,
+        default: 0
+      }
+    },
+    watch: {
+      value(val) {
+        if (this.currentValue !== val) {
+          this.currentValue = val
+        }
+      },
+      currentValue(val) {
+        this.$emit('input', val)
+      },
+    },
+    data() {
+      return {
+        currentValue: this.value,
+        indicatorLeft: 0,
+        indicatorWidth: 100,
+        timer: null,
+      }
+    },
+    methods: {
+      _handleMouseEnter(e) {
+        if (!!this.timer) {
+          clearTimeout(this.timer)
+          this.timer = null
+        }
+        this.indicatorLeft = e.target.offsetLeft
+        this.indicatorWidth = e.target.offsetWidth
+      },
+      _handleMouseLeave(e) {
+        if (!!this.timer) {
+          clearTimeout(this.timer)
+          this.timer = null
+        }
+        this.timer = setTimeout(() => {
+          this.indicatorLeft = this.$refs.items[this.currentValue].offsetLeft
+          this.indicatorWidth = this.$refs.items[this.currentValue].offsetWidth
+        }, 100)
+      },
+      _handleClickItem(item, index) {
+        this.currentValue = index
+        this.$emit('change', {item, index})
+      },
+    },
+    mounted() {
+      let el = this.$refs.items[this.currentValue]
+      this.indicatorLeft = el.offsetLeft
+      this.indicatorWidth = el.offsetWidth
+    },
+    computed: {
+      indicatorStyle() {
+        return {
+          left: `${this.indicatorLeft}px`,
+          width: `${this.indicatorWidth}px`,
+          backgroundColor: this.indicatorColor,
+          height: `${this.indicatorSize}px`,
+          borderRadius: `${this.indicatorSize}px`,
+        }
+      },
+    }
+  }
+</script>
+
+<style lang="scss">
+  .a-tabs-head {
+    position: relative;
+    padding-left: 12px;
+    height: 32px;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    .a-tabs-head-item {
+      height: 100%;
+      display: inline-flex;
+      align-items: center;
+      margin-right: 12px;
+      user-select: none;
+    }
+    .a-tabs-head-indicator {
+      transition: all 0.15s;
+      position: absolute;
+      border-radius: 2px;
+      bottom: 0;
+
+    }
+  }
+</style>
--- src/base/components/a-tabs/a-tabs.vue
+++ src/base/components/a-tabs/a-tabs.vue
@@ -1,22 +1,40 @@
 <template>
   <div class="a-tabs">
-    <div class="a-tabs-head">
-      this is tabs head
-    </div>
-    <div ref="content">
-      tabs content
-    </div>
+    <a-tabs-head :labels="tabLabels" @change="_handleChangeLabel"/>
+    <a-carousel
+      ref="carousel"
+      :swipeable="false"
+      height="100px">
+      <div
+        class="a-carousel-item item-cls"
+        v-for="(item,index) in list"
+        :style="{backgroundColor:item.color}"
+      >
+        <div>{{item.name}}--{{index}}</div>
+      </div>
+    </a-carousel>
   </div>
 </template>
 
 <script>
   import Vue from 'vue'
+  import ATabsHead from "./a-tabs-head";
+  import ACarousel from "../a-carousel/a-carousel";
 
   export default {
     name: "a-tabs",
+    components: {ACarousel, ATabsHead},
     data() {
       return {
-        vueCopmponents: []
+        vueComponents: [],
+
+        list: [
+          {name: 'input', color: '#abf', width: '100px', initialized: false},
+          {name: 'button', color: '#ff7261', width: '200px', initialized: false},
+          {name: 'textarea', color: '#598dff', width: '300px', initialized: false},
+          {name: 'hello', color: '#4effbc', width: '200px', initialized: false},
+          {name: 'balbala', color: '#ff24ad', width: '200px', initialized: false},
+        ],
       }
     },
     methods: {
@@ -24,13 +42,25 @@
         let instance = new Vue(component)
         const vueComponent = instance.$mount()
         console.log(vueComponent)
-        this.vueCopmponents.push(vueComponent)
+        this.vueComponents.push(vueComponent)
         this.$refs.content.appendChild(vueComponent.$el)
       },
       removeTab() {
-        let vc = this.vueCopmponents.pop()
+        let vc = this.vueComponents.pop()
         this.$refs.content.removeChild(vc.$el)
         vc.$destroy()
+      },
+
+      _handleChangeLabel({item, index}) {
+        console.log(item, index)
+        this.$refs.carousel.scrollTo(index)
+      },
+    },
+    computed: {
+      tabLabels() {
+        return [
+          '西红柿', '牙膏', '烙饼', '杂酱面', '烧烤摊'
+        ]
       },
     }
   }
@@ -39,11 +69,6 @@
 <style lang="scss">
   .a-tabs {
     text-align: left;
-    .a-tabs-head {
-      background-color: $text-color-primary;
-      color: white;
-      height: 32px;
-      line-height: 32px;
-    }
+
   }
 </style>
