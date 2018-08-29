6dfd67a1a202199943b1d0fe8e442fa0c5dd8110
martSforever <marts_forever@163.com>
2018-08-29 23:41:06 +0800

    a-notice-container 懒加载

--- src/base/components/a-notice/index.js
+++ src/base/components/a-notice/index.js
@@ -1,17 +1,6 @@
 import NoticeContainer from './a-notice-container'
 
-
-const noticeContainers = {
-  'start-start': NoticeContainer.newInstance({horizontal: 'start', vertical: 'start'}),
-  'start-center': NoticeContainer.newInstance({horizontal: 'start', vertical: 'center'}),
-  'start-end': NoticeContainer.newInstance({horizontal: 'start', vertical: 'end'}),
-  'center-start': NoticeContainer.newInstance({horizontal: 'center', vertical: 'start'}),
-  'center-center': NoticeContainer.newInstance({horizontal: 'center', vertical: 'center'}),
-  'center-end': NoticeContainer.newInstance({horizontal: 'center', vertical: 'end'}),
-  'end-start': NoticeContainer.newInstance({horizontal: 'end', vertical: 'start'}),
-  'end-center': NoticeContainer.newInstance({horizontal: 'end', vertical: 'center'}),
-  'end-end': NoticeContainer.newInstance({horizontal: 'end', vertical: 'end'}),
-}
+const noticeContainers = {}
 
 const DefaultNoticeOptions = {
   vertical: 'end',
@@ -23,7 +12,12 @@ const DefaultNoticeOptions = {
 export default {
   info(options) {
     let o = Object.assign({}, DefaultNoticeOptions, options)
-    noticeContainers[`${o.horizontal}-${o.vertical}`].addNotice(o)
+    let noticeContainer = noticeContainers[`${o.horizontal}-${o.vertical}`]
+    if (!noticeContainer) {
+      noticeContainer = NoticeContainer.newInstance({horizontal: o.horizontal, vertical: o.vertical})
+      noticeContainers[`${o.horizontal}-${o.vertical}`] = noticeContainer
+    }
+    noticeContainer.addNotice(o)
   },
 }
 
