28d036043be84bc13bd33e7c2697343c4712047e
martsforever <martsforever@aliyun.com>
2018-08-31 14:13:01 +0800

    a-table-head-cell增加功能，当鼠标移动到底部标题的后边框的时候，鼠标变为扩展指针

--- src/base/components/a-table/a-table-head-cell.vue
+++ src/base/components/a-table/a-table-head-cell.vue
@@ -3,7 +3,8 @@
       :rowspan="rowspan"
       :colspan="colspan"
       @mousemove="_handleMouseMove"
-      @mouseleave="_handleMouseLeave">
+      @mouseleave="_handleMouseLeave"
+      ref="td">
     <!--不能给td设置宽度，当列宽总和大于容器宽度是，table列会被压缩，设置里面的div的宽度即可-->
     <div class="cell" :style="_getHeadTdCellStyles(column)">
       <rendering-scope-slot v-if="!!column.titleScopedSlots" :scope-slot-func="column.titleScopedSlots"/>
@@ -43,11 +44,22 @@
         return ret
       },
       _handleMouseMove(e) {
-
+        if (!this.isBottom) return
+        let {offsetX} = e
+        let offsetWidth = this.$refs.td.offsetWidth
+        document.body.style.cursor = Math.abs(offsetX - offsetWidth + 5) < 5 ? 'w-resize' : 'default'
       },
       _handleMouseLeave(e) {
-
+        if (!this.isBottom) return
+        document.body.style.cursor = 'default'
       },
+    },
+    computed: {
+      /*是否为最下级表头，就是没有子表头的*/
+      isBottom() {
+        let children = this.column.children
+        return !(!!children && children.length > 0)
+      }
     },
   }
 </script>
