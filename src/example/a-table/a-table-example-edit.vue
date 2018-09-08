1e9a54d691cecb831fd61fb956d5555d63f01a15
martsforever <martsforever@aliyun.com>
2018-09-08 09:41:35 +0800

    示例代码调整

--- src/example/a-table/a-table-example-edit.vue
+++ src/example/a-table/a-table-example-edit.vue
@@ -2,18 +2,27 @@
   <div class="a-table-example-edit">
 
     <div>
-      <a-button-group>
+      <div class="operation">
+        <div class="item">
+          单行编辑：
+          <a-switch v-model="multiEditable"/>
+        </div>
+
         <a-button-group>
-          <a-button @click="saveTableEditData">save edit</a-button>
-          <a-button @click="cancelEdit">cancel edit</a-button>
+          <a-button-group>
+            <a-button @click="saveTableEditData">save edit</a-button>
+            <a-button @click="cancelEdit">cancel edit</a-button>
+          </a-button-group>
         </a-button-group>
-      </a-button-group>
+      </div>
+
     </div>
 
     <div>
       <a-table
         ref="table"
         :list="dataList"
+        :multi-editable="multiEditable"
       >
         <!--文本输入列-->
         <a-table-column-input field="name" title="姓名"/>
@@ -29,12 +38,14 @@
   import AButton from "../../base/components/a-button/a-button";
   import ATable from "../../base/components/a-table/a-table";
   import ATableColumnInput from "../../base/components/a-table/custome/input/a-table-column-input";
+  import ASwitch from "../../base/components/a-switch/a-switch";
 
   export default {
     name: "a-table-example-edit",
-    components: {ATableColumnInput, ATable, AButton, AButtonGroup},
+    components: {ASwitch, ATableColumnInput, ATable, AButton, AButtonGroup},
     data() {
       return {
+        multiEditable: false,
         dataList: [
           {name: '渣渣辉', code: '0x001'},
           {name: '刘德华', code: '0x002'},
@@ -62,5 +73,15 @@
 <style lang="scss">
   .a-table-example-edit {
     height: 100%;
+    .operation {
+      display: inline-flex;
+      align-items: center;
+      padding: 24px;
+      .item {
+        display: inline-flex;
+        align-items: center;
+        margin-right: 24px;
+      }
+    }
   }
 </style>
