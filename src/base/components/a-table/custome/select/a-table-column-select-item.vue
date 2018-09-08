23f603b3512983fcb496762fdaa4836349cf44f9
martSforever <marts_forever@163.com>
2018-09-08 22:54:54 +0800

    新增a-table-column-select

---
+++ src/base/components/a-table/custome/select/a-table-column-select-item.vue
@@ -0,0 +1,21 @@
+<template>
+  <div ref="wrapper">
+    <keep-alive>
+      <a-select v-model="currentValue" v-if="!!editable && !!currentEditable">
+
+      </a-select>
+      <span v-else>{{row[field]}}</span>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+  import TableEditMixin from "../table-edit-mixin";
+  import ASelect from "../../../a-select/a-select";
+
+  export default {
+    name: "a-table-column-select-item",
+    components: {ASelect},
+    mixins: [TableEditMixin],
+  }
+</script>
---
+++ src/base/components/a-table/custome/select/a-table-column-select.vue
@@ -0,0 +1,45 @@
+<template>
+  <a-table-column :title="title" ref="tableColumn" :width="width">
+    <template slot-scope="{row,rowIndex}">
+
+    </template>
+  </a-table-column>
+</template>
+
+<script>
+  import ATableColumn from "../../a-table-column";
+
+  export default {
+    name: "a-table-column-select",
+    components: {ATableColumn},
+    props: {
+      title: {
+        default: '选择日期'
+      },
+      order: {
+        default: 0
+      },
+      field: {
+        type: String,
+        required: true
+      },
+      width: {
+        default: '160px'
+      },
+    },
+    data() {
+      return {
+        isTableColumn: true,
+      }
+    },
+    methods: {
+      getColumn() {
+        return this.$refs.tableColumn.getColumn()
+      },
+    },
+  }
+</script>
+
+<style lang="scss">
+
+</style>
